971e23bd3c6c1eb8619a573dfcb0745961605d6f
MythMega <45936113+MythMega@users.noreply.github.com>
2022-04-04 14:00:27 +0200

    add animation

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
         <label for="inputName" class="txtColored"><strong>Your name</strong></label>
         <input class="form-control form-control-lg" type="text" placeholder="Ed'" aria-label=".form-control-lg example" id="inputName" name="inputName">
         <label for="inputName" class="txtColored"><strong>Your message</strong></label>
-        <input class="form-control form-control-lg" type="text" placeholder="I loved it" aria-label=".form-control-lg example" id="inputMsg" name="inputMsg">
+          <label for="" class="form-label"></label>
+          <textarea class="form-control  form-control-lg" name="" id="" rows="3" placeholder="I loved it because..." aria-label=".form-control-lg example" id="inputMsg" name="inputMsg"></textarea>
         <button type="submit" class="btn btn-primary mb-3 formButton">Send</button>
     </form>
 </div>
