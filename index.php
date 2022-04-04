40b5358abb83f186e16999bfea4174c490254f59
MythMega <45936113+MythMega@users.noreply.github.com>
2022-04-04 15:16:36 +0200

    Button to go list

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
         <h3 class="txtColored">Journey's GoldenBook</h3>
         <label for="inputName" class="txtColored"><strong>Your name</strong></label>
         <input class="form-control form-control-lg" type="text" placeholder="Ed'" aria-label=".form-control-lg example" id="inputName" name="inputName" minlength="5" maxlength="50" pattern=".{5,}" required>
-        <label for="inputName" class="txtColored"><strong>Your message</strong></label>
+        <label for="inputName" class="txtColored"><strong>Message (optional)</strong></label>
         <textarea class="form-control  form-control-lg" name="inputMsg" id="inputMsg" rows="3" placeholder="I loved it because..." maxlength="511" aria-label=".form-control-lg example"></textarea>
         <button type="submit" class="btn btn-primary mb-3 formButton">Send</button>
     </form>
+    <a href="list.php"><button class="btn btn-primary mb-3 formButton">See List</button></a>
 </div>
 
 
