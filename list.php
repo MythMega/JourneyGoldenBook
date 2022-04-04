7f5f4b9a09c2e625c6ea9211d9982ffc02816fc4
MythMega <45936113+MythMega@users.noreply.github.com>
2022-04-04 12:06:32 +0200

    Working (but bad) version

---
+++ list.php
@@ -0,0 +1,52 @@
+<?php include'./file/php/head.php'; ?>
+
+<div class="content_big">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Message</th>
+                        <th scope="col">date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Connexion à la base de données
+                        try
+                        {
+                            $bdd = new PDO('mysql:host=localhost;dbname=journeybook;charset=utf8', 'admin', 'admin');
+                        }
+                        catch(Exception $e)
+                        {
+                                die('Erreur : '.$e->getMessage());
+                        }
+
+                        // On récupère les 25 derniers billets
+                        $req = $bdd->query('SELECT id, pseudo, message, DATE_FORMAT(date, \'%d/%m/%Y\') AS date_creation FROM messages ORDER BY date DESC');
+
+                        while ($donnees = $req->fetch())
+                        {
+                        ?>
+                            <tr>
+                                    <td><?php echo htmlspecialchars($donnees['id']); ?></span></td>
+                                    <td><?php echo htmlspecialchars($donnees['pseudo']); ?></span></td>
+                                    <td><?php echo htmlspecialchars($donnees['message']); ?></span></td>
+                                    <td><?php echo htmlspecialchars($donnees['date_creation']); ?></span></td>
+                            </tr>
+                        <?php
+                        } // Fin de la boucle des billets
+                        $req->closeCursor();
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include'./file/php/foot.php'; ?>
---
+++ traitement.php
@@ -0,0 +1,33 @@
+<?php
+date_default_timezone_set('Europe/Paris');
+
+$name=$_POST['inputName'];
+$message=$_POST['inputMsg'];
+//$dateMsg = date('m/d/Y');
+
+
+
+$servername = "localhost";
+$username = "admin";
+$password = "admin";
+$dbname = "journeybook";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO messages (pseudo, message)
+VALUES ('$name', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+  header('Location: list.php');
+  exit();
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
