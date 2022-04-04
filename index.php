6317d5738d91660f75bf908c451936fc60bc1042
MythMega <45936113+MythMega@users.noreply.github.com>
2022-04-04 14:07:28 +0200

    add minimum lenght input

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <div class="content">
     <form action="traitement.php" method="POST">
         <h3 class="txtColored">Journey's GoldenBook</h3>
-        <label for="inputName" class="txtColored"><strong>Your name</strong></label>
+        <label for="inputName" class="txtColored" minlength="5" maxlength="50"><strong>Your name</strong></label>
         <input class="form-control form-control-lg" type="text" placeholder="Ed'" aria-label=".form-control-lg example" id="inputName" name="inputName">
         <label for="inputName" class="txtColored"><strong>Your message</strong></label>
           <label for="" class="form-label"></label>
-          <textarea class="form-control  form-control-lg" name="" id="" rows="3" placeholder="I loved it because..." aria-label=".form-control-lg example" id="inputMsg" name="inputMsg"></textarea>
+          <textarea class="form-control  form-control-lg" name="inputMsg" id="inputMsg" rows="3" placeholder="I loved it because..." maxlength="511" aria-label=".form-control-lg example"></textarea>
         <button type="submit" class="btn btn-primary mb-3 formButton">Send</button>
     </form>
 </div>
